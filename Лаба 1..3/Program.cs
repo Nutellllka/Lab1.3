782f753635e71039be71b8e273c18a93ef5be719
Vlada <Guzeieiva.Vlada@lll.kpi.ua>
2023-03-15 00:41:34 +0300

    Initial commit

---
+++ Лаба 1..3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Лаба_1.__3
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("3 завдання");
+          
+            int lght = 4;
+
+            List<string> line = new List<string>()
+            {
+                "FDAS",
+                "GFRS",
+                "DSRE",
+                "LKOM",
+                "TREV"
+            };
+
+            var selectedLine = line.Where(l => l.Length == lght).OrderByDescending(l=>l).FirstOrDefault();
+          
+            Console.WriteLine(selectedLine);
+        }
+    }
+}
